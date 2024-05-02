42beb79a0ce27a68d88235aeb346c2c0d3058d94
Ravi Vataliya <vataliyag174@gmail.com>
2024-05-02 21:32:22 +0530

    Update Task Manager System

--- PROJECTS/Task Manager/index.js
+++ PROJECTS/Task Manager/index.js
@@ -14,6 +14,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "src")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
