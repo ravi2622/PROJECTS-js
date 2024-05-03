4caad5cab64b6e46226243af00a8aac3623f734c
Ravi Vataliya <vataliyag174@gmail.com>
2024-05-03 16:10:02 +0530

    Update Task-Manager System

--- PROJECTS/Task Manager/index.js
+++ PROJECTS/Task Manager/index.js
@@ -31,11 +31,25 @@ app.get("/show", (req, res) => {
     return res.status(400).render("404.html");
 });
 
-app.get("/taskmanager", (req, res) => {
-    res.render("index.ejs");
+app.get("/show/:id", (req, res) => {
+    let { id } = req.params;
+
+    let p = tasks.find((p) => p.id === id);
+    if (p) {
+        res.render("show.ejs", { p });
+    }
+    else {
+        return res.status(400).render("404.html");
+    };
 });
 
-app.post("/show", (req, res) => {
+app.get("/taskmanager", (req, res) => {
+    tasks.forEach((p) => {
+        res.render("index.ejs", { p });
+    })
+});
+
+app.post("/show/:id", (req, res) => {
     let { id, username } = req.body;
 
     console.log(id);
@@ -49,19 +63,19 @@ app.post("/show", (req, res) => {
     else {
         return res.status(400).render("404.html");
     };
-    
+
     // logic - 2    
     // let task = tasks.filter((P) => {
-        //     if (id === P.id) {
-            //         return P;
-            //     };
-            // });
-            // console.log(task);
-            // console.log(task[0]);
-            // let p = task[0];
-            // if (p) {
-                //     if (username === p.username) {
-                    //         res.render("show.ejs", { p });
+    //     if (id === P.id) {
+    //         return P;
+    //     };
+    // });
+    // console.log(task);
+    // console.log(task[0]);
+    // let p = task[0];
+    // if (p) {
+    //     if (username === p.username) {
+    //         res.render("show.ejs", { p });
     //     }
     // }
     // else {
@@ -76,27 +90,27 @@ app.get("/newuser", (req, res) => {
 });
 
 app.post("/taskmanager", (req, res) => {
-    let {username, id} = req.body;
-    let {tasklist} = req.body;
+    let { username, id } = req.body;
+    let { tasklist } = req.body;
     let task = [];
     console.log(tasklist);
     tasklist.forEach((P) => {
         console.log(P);
-        if(P != "") {
+        if (P != "") {
             task.push(P);
         }
     });
     console.log(task);
-    tasks.push({username, id, task});
+    tasks.push({ username, id, task });
     res.redirect("/taskmanager");
 });
 
-app.get("/show/:id", (req, res) =>  {
+app.get("/show/:id/newtask", (req, res) => {
     let { id } = req.params;
     console.log(id);
     let task = tasks.find((p) => p.id === id);
     console.log(task);
-    if(task) {
+    if (task) {
         res.render("newtask.ejs", { task });
     }
     else {
@@ -104,15 +118,24 @@ app.get("/show/:id", (req, res) =>  {
     };
 });
 
-app.post("/taskmanager", (req, res) => {
-    res.redirect("/taskmanager");
+app.patch("/show/:id", (req, res) => {
+    let { id } = req.params;
+
+    let task = tasks.find((P) => P.id === id);
+    let edittask = req.body.tasklist;
+    console.log(edittask);
+
+    // let neweditedtask = edittask.filter((task) => (task != ""));
+    task.task = edittask.filter((task) => (task != ""));
+
+    res.redirect(`/show/${id}`);
 });
 
 let tasks = [
     {
         id: "a1",
         username: "Ravi_vataliya",
-        task: ["sleep","eat","read"]
+        task: ["sleep", "eat", "read"]
     },
     {
         id: "a2",
---
+++ PROJECTS/Task Manager/public/newtask.js
@@ -0,0 +1,16 @@
+let taskclear = document.querySelector(".taskclear");
+let tasklist = document.querySelector("#tasklist");
+let ul = document.querySelector("ul");
+
+taskclear.addEventListener("click", () => {
+    let li = document.createElement("li");
+    let newinput = document.createElement("input");
+    
+    newinput.setAttribute("type", "text");
+    newinput.setAttribute("name", "tasklist");
+    newinput.setAttribute("id", "tasklist");
+    newinput.setAttribute("placeholder", "Enter New Task");
+    li.appendChild(newinput);
+    ul.appendChild(li);
+});
+
